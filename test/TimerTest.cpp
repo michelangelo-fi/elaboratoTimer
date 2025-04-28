ea5124f87e3f37f7f65284653ae116587b649cc6
michelangelo <michelangelo.corsari@edu.unifi.it>
2025-04-28 14:47:43 +0200

    implementato e provato unit test per timer

--- test/TimerTest.cpp
+++ test/TimerTest.cpp
@@ -1,4 +1,14 @@
-//
-// Created by michelangelo on 28/04/25.
-//
+
 #include "Timer.h"
+#include <gtest/gtest.h>
+
+TEST(TimerTest, Countdown){
+    Timer t; t.start(3);
+    t.tick(); // partendo da 3 mi rimarranno 2 secondi
+    EXPECT_EQ(t.remaining(),2);
+    t.tick(); // 1
+    t.tick(); // 0
+    EXPECT_TRUE(t.isFinished());
+    EXPECT_EQ(t.remainingStr(), "00:00:00");
+}
+
